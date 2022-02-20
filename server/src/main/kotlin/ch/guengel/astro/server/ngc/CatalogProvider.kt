d92d5fe0982aba77a32f72ca4ba7c8d23268d638
Rafael Ostertag <rafi@guengel.ch>
2022-02-20 12:16:05 +0100

    Log when loading catalog

--- server/src/main/kotlin/ch/guengel/astro/server/ngc/CatalogProvider.kt
+++ server/src/main/kotlin/ch/guengel/astro/server/ngc/CatalogProvider.kt
@@ -3,6 +3,7 @@ package ch.guengel.astro.server.ngc
 import ch.guengel.astro.openngc.Catalog
 import ch.guengel.astro.openngc.parser.CSVParser
 import ch.guengel.astro.server.common.ngc.CatalogFileService
+import org.jboss.logging.Logger
 import java.time.OffsetDateTime
 import javax.enterprise.context.ApplicationScoped
 
@@ -13,6 +14,7 @@ class CatalogProvider(private val catalogFileService: CatalogFileService) {
      * Load catalog. If the catalog does not exist, it will be fetched.
      */
     fun loadCatalog(): Catalog {
+        log.info("Load catalog from ${catalogFileService.catalogFile}")
         return CSVParser.parse(catalogFileService.catalogFile)
     }
 
@@ -20,5 +22,9 @@ class CatalogProvider(private val catalogFileService: CatalogFileService) {
      * Get date and time of last catalog update
      */
     fun getLastUpdated(): OffsetDateTime? = catalogFileService.lastUpdated
+
+    private companion object {
+        private val log: Logger = Logger.getLogger(CatalogProvider::class.java)
+    }
 
 }
